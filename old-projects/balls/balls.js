b997cea4cd483db756c2e7f639ad4e4aeda3cd17
Vlad F <50790065+vladf1@users.noreply.github.com>
2026-05-29 13:24:34 -0700

    modernize

--- old-projects/balls/balls.js
+++ old-projects/balls/balls.js
@@ -1,183 +1,198 @@
-var Ball = (function () {
-    function Ball(originalX, originalY, originalRadius) {
+class Ball {
+    static maxVelocity = 0.5;
+    static twoPi = Math.PI * 2;
+
+    constructor(originalX, originalY, originalRadius) {
         this.angle = this.getRandomValueInRange(0, Ball.twoPi);
         this.dx = Ball.maxVelocity * Math.cos(this.angle);
         this.dy = Ball.maxVelocity * Math.sin(this.angle);
         this.x = originalX;
         this.y = originalY;
         this.radius = originalRadius;
-        var r = Math.floor(this.getRandomValueInRange(10, 255));
-        var g = Math.floor(this.getRandomValueInRange(10, 255));
-        var b = Math.floor(this.getRandomValueInRange(10, 255));
-        this.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+
+        const r = Math.floor(this.getRandomValueInRange(10, 255));
+        const g = Math.floor(this.getRandomValueInRange(10, 255));
+        const b = Math.floor(this.getRandomValueInRange(10, 255));
+        this.fillStyle = `rgb(${r},${g},${b})`;
     }
-    Ball.prototype.getRandomValueInRange = function (minVal, maxVal) {
-        return minVal + (Math.random() * (maxVal - minVal));
-    };
-    Ball.prototype.isOutsideDistance = function (x2, y2, dist) {
-        var xdif = Math.abs(this.x - x2);
-        if (dist <= xdif)
+
+    getRandomValueInRange(minVal, maxVal) {
+        return minVal + Math.random() * (maxVal - minVal);
+    }
+
+    isOutsideDistance(x2, y2, dist) {
+        const xdif = Math.abs(this.x - x2);
+        if (dist <= xdif) {
             return true;
-        var ydif = Math.abs(this.y - y2);
-        if (dist <= ydif)
+        }
+
+        const ydif = Math.abs(this.y - y2);
+        if (dist <= ydif) {
             return true;
-        var sumOfSquares = Math.pow(xdif, 2) + Math.pow(ydif, 2);
-        var sqrDist = Math.pow(dist, 2);
+        }
+
+        const sumOfSquares = xdif ** 2 + ydif ** 2;
+        const sqrDist = dist ** 2;
         return sqrDist <= sumOfSquares;
-    };
-    Ball.prototype.checkForCollisionsWithOtherBalls = function (balls) {
-        var _this = this;
-        balls.forEach(function (otherBall) {
-            if (otherBall === _this)
-                return;
-            var collided = _this.isCollided(otherBall.x, otherBall.y, otherBall.radius);
-            if (!collided)
-                return;
-            var combinedSpeed = _this.getSpeed() + otherBall.getSpeed() * .98; // some loss of speed from collision
-            var thisBallRatio = 1 - _this.radius / (_this.radius + otherBall.radius);
-            var newSpeed = combinedSpeed * thisBallRatio;
-            _this.angle = Math.atan2(_this.y - otherBall.y, _this.x - otherBall.x);
-            _this.dx = newSpeed * Math.cos(_this.angle);
-            _this.dy = newSpeed * Math.sin(_this.angle);
-        });
-    };
-    Ball.prototype.getSpeed = function () {
-        return Math.sqrt(this.dx * this.dx + this.dy * this.dy);
-    };
-    Ball.prototype.flyAway = function (x, y, minDistance, speed) {
-        var collided = this.isCollided(x, y, minDistance);
+    }
+
+    checkForCollisionsWithOtherBalls(balls) {
+        for (const otherBall of balls) {
+            if (otherBall === this) {
+                continue;
+            }
+
+            const collided = this.isCollided(otherBall.x, otherBall.y, otherBall.radius);
+            if (!collided) {
+                continue;
+            }
+
+            const combinedSpeed = this.getSpeed() + otherBall.getSpeed() * 0.98; // some loss of speed from collision
+            const thisBallRatio = 1 - this.radius / (this.radius + otherBall.radius);
+            const newSpeed = combinedSpeed * thisBallRatio;
+
+            this.angle = Math.atan2(this.y - otherBall.y, this.x - otherBall.x);
+            this.dx = newSpeed * Math.cos(this.angle);
+            this.dy = newSpeed * Math.sin(this.angle);
+        }
+    }
+
+    getSpeed() {
+        return Math.hypot(this.dx, this.dy);
+    }
+
+    flyAway(x, y, minDistance, speed) {
+        const collided = this.isCollided(x, y, minDistance);
         if (collided) {
-            this.angle = Math.atan2(this.y - x, this.x - x);
+            this.angle = Math.atan2(this.y - y, this.x - x);
             this.dx = speed * Math.cos(this.angle);
             this.dy = speed * Math.sin(this.angle);
         }
-    };
-    Ball.prototype.isCollided = function (x, y, otherObjectRadius) {
-        var distance = this.radius + otherObjectRadius;
-        var collided = !this.isOutsideDistance(x, y, distance);
-        return collided;
-    };
-    Ball.prototype.calculateNewPosition = function (millisecondsSinceLastRender, canvasWidth, canvasHeight) {
-        var changeY = millisecondsSinceLastRender * this.dy;
-        var changeX = millisecondsSinceLastRender * this.dx;
+    }
+
+    isCollided(x, y, otherObjectRadius) {
+        const distance = this.radius + otherObjectRadius;
+        return !this.isOutsideDistance(x, y, distance);
+    }
+
+    calculateNewPosition(millisecondsSinceLastRender, canvasWidth, canvasHeight) {
+        let changeY = millisecondsSinceLastRender * this.dy;
+        let changeX = millisecondsSinceLastRender * this.dx;
+
         if (this.y + changeY - this.radius < 0) {
             this.y = this.radius;
             changeY *= -1;
             this.dy *= -1;
+        } else if (this.y + this.dy + this.radius > canvasHeight) {
+            this.y = canvasHeight - this.radius;
+            this.dy *= -1;
+            changeY *= -1;
         }
-        else {
-            if (this.y + this.dy + this.radius > canvasHeight) {
-                this.y = canvasHeight - this.radius;
-                this.dy *= -1;
-                changeY *= -1;
-            }
-        }
+
         if (this.x + changeX - this.radius < 0) {
             this.x = this.radius;
             this.dx *= -1;
             changeX *= -1;
+        } else if (this.x + changeX + this.radius > canvasWidth) {
+            this.x = canvasWidth - this.radius;
+            this.dx *= -1;
+            changeX *= -1;
         }
-        else {
-            if (this.x + changeX + this.radius > canvasWidth) {
-                this.x = canvasWidth - this.radius;
-                this.dx *= -1;
-                changeX *= -1;
-            }
-        }
-        this.y += changeY; // move
+
+        this.y += changeY;
         this.x += changeX;
-    };
-    Ball.prototype.draw = function (context) {
+    }
+
+    draw(context) {
         context.fillStyle = this.fillStyle;
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Ball.twoPi, false);
         context.closePath();
         context.fill();
-    };
-    Ball.maxVelocity = .5;
-    Ball.twoPi = Math.PI * 2;
-    return Ball;
-})();
-var Scene = (function () {
-    function Scene() {
-        var _this = this;
-        this.balls = new Array();
+    }
+}
+
+class Scene {
+    constructor() {
+        this.balls = [];
         this.lastAnimated = 0;
-        var statusBarElement = document.getElementById("statusBar");
-        this.frameCounter = new FrameCounter(function (frames, updates) {
-            statusBarElement.innerHTML = frames + " frames / sec, " + updates + " updates / sec";
+
+        const statusBarElement = document.getElementById("statusBar");
+        this.frameCounter = new FrameCounter((frames, updates) => {
+            statusBarElement.innerHTML = `${frames} frames / sec, ${updates} updates / sec`;
         });
-        var nowOffset = Date.now(); // in case performance.now is not supported
-        this.getNow = window.performance && window.performance.now ? function () { return window.performance.now(); } : function () { return Date.now() - nowOffset; };
-        var canvas = document.getElementById("canvas");
+
+        const canvas = document.getElementById("canvas");
         this.context = canvas.getContext("2d");
-        window.onresize = function () {
-            _this.width = canvas.width = document.documentElement.clientWidth;
-            _this.height = canvas.height = document.documentElement.clientHeight;
+
+        window.onresize = () => {
+            this.width = canvas.width = document.documentElement.clientWidth;
+            this.height = canvas.height = document.documentElement.clientHeight;
         };
-        window.onresize(null);
-        canvas.onclick = function (e) {
-            _this.balls.forEach(function (b) {
-                b.flyAway(e.clientX, e.clientY, 900, .5); // fly away faster
-            });
+        window.onresize();
+
+        canvas.onclick = (e) => {
+            for (const ball of this.balls) {
+                ball.flyAway(e.clientX, e.clientY, 900, 0.5); // fly away faster
+            }
         };
-        var numberOfBalls = 35;
-        for (var i = 0; i < numberOfBalls; i++) {
+
+        const numberOfBalls = 35;
+        for (let i = 0; i < numberOfBalls; i++) {
             this.tryToCreateBallInEmptySpace();
         }
     }
-    Scene.prototype.getRandomValueInRange = function (minVal, maxVal) {
-        return minVal + (Math.random() * (maxVal - minVal));
-    };
-    Scene.prototype.tryToCreateBallInEmptySpace = function () {
-        for (var attempt = 0; attempt < 50; attempt++) {
-            var radius = this.getRandomValueInRange(15, 45);
-            var margin = radius + 10;
-            var startPosX = this.getRandomValueInRange(margin, this.width - margin);
-            var startPosY = this.getRandomValueInRange(margin, this.height - margin);
-            var notCollidedWithAnyExistingBall = true;
-            for (var i = 0; i < this.balls.length; i++) {
-                var otherBubble = this.balls[i];
-                var collided = otherBubble.isCollided(startPosX, startPosY, radius);
-                if (collided) {
-                    notCollidedWithAnyExistingBall = false;
-                    break;
-                }
-            }
-            if (notCollidedWithAnyExistingBall) {
-                var newBall = new Ball(startPosX, startPosY, radius);
-                this.balls.push(newBall);
+
+    getRandomValueInRange(minVal, maxVal) {
+        return minVal + Math.random() * (maxVal - minVal);
+    }
+
+    tryToCreateBallInEmptySpace() {
+        for (let attempt = 0; attempt < 50; attempt++) {
+            const radius = this.getRandomValueInRange(15, 45);
+            const margin = radius + 10;
+            const startPosX = this.getRandomValueInRange(margin, this.width - margin);
+            const startPosY = this.getRandomValueInRange(margin, this.height - margin);
+
+            const collidedWithExistingBall = this.balls.some((otherBubble) => {
+                return otherBubble.isCollided(startPosX, startPosY, radius);
+            });
+
+            if (!collidedWithExistingBall) {
+                this.balls.push(new Ball(startPosX, startPosY, radius));
                 return;
             }
         }
-    };
-    Scene.prototype.renderScene = function () {
-        var _this = this;
+    }
+
+    renderScene() {
         this.context.clearRect(0, 0, this.width, this.height); // erase the canvas
-        this.balls.forEach(function (b) {
-            b.draw(_this.context);
-        });
+        for (const ball of this.balls) {
+            ball.draw(this.context);
+        }
         this.frameCounter.frameRendered();
-        requestAnimationFrame(function () { return _this.renderScene(); });
-    };
-    Scene.prototype.updateScene = function () {
-        var _this = this;
-        var now = this.getNow();
-        var millisecondsSinceLastRender = this.lastAnimated !== 0 ? now - this.lastAnimated : 0;
+        requestAnimationFrame(() => this.renderScene());
+    }
+
+    updateScene() {
+        const now = performance.now();
+        const millisecondsSinceLastRender = this.lastAnimated !== 0 ? now - this.lastAnimated : 0;
         this.lastAnimated = now;
-        this.balls.forEach(function (b) {
-            b.checkForCollisionsWithOtherBalls(_this.balls);
-        });
-        this.balls.forEach(function (b) {
-            b.calculateNewPosition(millisecondsSinceLastRender, _this.width, _this.height);
-        });
+
+        for (const ball of this.balls) {
+            ball.checkForCollisionsWithOtherBalls(this.balls);
+        }
+        for (const ball of this.balls) {
+            ball.calculateNewPosition(millisecondsSinceLastRender, this.width, this.height);
+        }
+
         this.frameCounter.updateComplete();
-        setTimeout(function () { return _this.updateScene(); });
-    };
-    return Scene;
-})();
-window.onload = function () {
-    var game = new Scene();
+        setTimeout(() => this.updateScene());
+    }
+}
+
+window.onload = () => {
+    const game = new Scene();
     game.updateScene();
     game.renderScene();
 };
