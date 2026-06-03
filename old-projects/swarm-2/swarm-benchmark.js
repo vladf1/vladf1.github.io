d76249857bff9235e767afcd3762fb9633c88e50
Vlad F <50790065+vladf1@users.noreply.github.com>
2026-06-02 22:51:14 -0700

    Default Swarm 2 to WebGPU and remove JS cache-busters

--- old-projects/swarm-2/swarm-benchmark.js
+++ old-projects/swarm-2/swarm-benchmark.js
@@ -10,7 +10,7 @@ import {
   createWebgpuComputeRenderer,
   createWebglRenderer,
   updateSprites
-} from "./swarm.js?v=50";
+} from "./swarm.js";
 
 const DEFAULT_FRAME_COUNT = 180;
 const DEFAULT_WIDTH = 800;
