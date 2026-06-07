51275cb3f3fb10679b27286fb815516a7bcf499c
Vlad F <50790065+vladf1@users.noreply.github.com>
2026-06-06 22:03:39 -0700

    Disable modulepreload polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
+    modulePreload: {
+      polyfill: false,
+    },
     rollupOptions: {
       input: {
         main: "index.html",
